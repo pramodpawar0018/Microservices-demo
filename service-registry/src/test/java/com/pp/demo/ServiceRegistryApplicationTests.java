7c0a8e72ca3c2d31965a5fbd218650124663a834
Dell <Dell@Pramod.mshome.net>
2024-09-22 23:19:46 +0530

    Service Registry

---
+++ service-registry/src/test/java/com/pp/demo/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
